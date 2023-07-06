624d0021abd0f6c88cdef44d1da50a038d5c1b89
Oksana Chernenko <senia@MacBook-Air-Oksana.local>
2023-07-06 20:04:15 +0300

    Implemented CRUD operations

--- src/main/java/com/chernenko/phonecontacs/service/ContactService.java
+++ src/main/java/com/chernenko/phonecontacs/service/ContactService.java
@@ -1,5 +1,6 @@
 package com.chernenko.phonecontacs.service;
 
+import com.chernenko.phonecontacs.dto.ContactDTO;
 import com.chernenko.phonecontacs.model.Contact;
 import com.chernenko.phonecontacs.repository.ContactRepository;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,23 @@ public class ContactService {
 
     private final ContactRepository contactRepository;
 
-    public List<Contact> findAll () {
+    public Contact create(ContactDTO contactDTO) {
+        return contactRepository.save(Contact.builder()
+                .name(contactDTO.getName())
+                .emails(contactDTO.getEmails())
+                .numbers(contactDTO.getNumbers())
+                .build());
+    }
+
+    public List<Contact> getAll () {
         return contactRepository.findAll();
     }
 
+    public Contact update(Contact contact) {
+        return contactRepository.save(contact);
+    }
 
+    public void delete(Long id) {
+        contactRepository.deleteById(id);
+    }
 }
