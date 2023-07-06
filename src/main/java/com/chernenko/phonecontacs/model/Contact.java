3eb15e48e893fcc3534d347a3d885391b3c98bdf
Oksana Chernenko <senia@MacBook-Air-Oksana.local>
2023-07-06 22:36:43 +0300

    Added Spring Security

--- src/main/java/com/chernenko/phonecontacs/model/Contact.java
+++ src/main/java/com/chernenko/phonecontacs/model/Contact.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class Contact {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
