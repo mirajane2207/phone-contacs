71bb6e4ce94419c680f1c69fbfed45609b35e0e6
Oksana Chernenko <senia@MacBook-Air-Oksana.local>
2023-07-06 20:22:10 +0300

    Implemented RestController

--- src/main/java/com/chernenko/phonecontacs/controller/ContactController.java
+++ src/main/java/com/chernenko/phonecontacs/controller/ContactController.java
@@ -1,13 +1,39 @@
 package com.chernenko.phonecontacs.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.chernenko.phonecontacs.dto.ContactDTO;
+import com.chernenko.phonecontacs.model.Contact;
+import com.chernenko.phonecontacs.service.ContactService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class ContactController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> create(@RequestBody ContactDTO contactDTO) {
+        return new ResponseEntity<>(contactService.create(contactDTO), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Contact>> getAll() {
+        return new ResponseEntity<>(contactService.getAll(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Contact> update(@RequestBody Contact contact) {
+        return new ResponseEntity<>(contactService.update(contact), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus delete(@PathVariable Long id) {
+        contactService.delete(id);
+        return HttpStatus.OK;
     }
 }
