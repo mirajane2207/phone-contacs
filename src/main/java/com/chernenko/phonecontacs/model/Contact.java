1bcce874f351c00304c0bb4e703aac8d64f244dc
Oksana Chernenko <senia@MacBook-Air-Oksana.local>
2023-07-06 16:41:03 +0300

    Lombok added to project

--- src/main/java/com/chernenko/phonecontacs/model/Contact.java
+++ src/main/java/com/chernenko/phonecontacs/model/Contact.java
@@ -1,73 +1,26 @@
 package com.chernenko.phonecontacs.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Contact {
     @Id
     @GeneratedValue
     private Long id;
-    private String emails;
 
-    public Contact(String emails, String number) {
-        this.emails = emails;
-        this.number = number;
-    }
+    @ElementCollection
+    private Set<String> emails;
 
-    public Contact() {
-
-    }
-
-    public String getEmails() {
-        return emails;
-    }
-
-    public void setEmails(String emails) {
-        this.emails = emails;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    private String number;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contact contact = (Contact) o;
-        return Objects.equals(id, contact.id) && Objects.equals(emails, contact.emails) && Objects.equals(number, contact.number);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, emails, number);
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "id=" + id +
-                ", emails='" + emails + '\'' +
-                ", number='" + number + '\'' +
-                '}';
-    }
+    @ElementCollection
+    private Set<String> numbers;
 }
